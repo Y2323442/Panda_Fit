add04e47acdd0da14b53bce2c188d9f72cae9440
曹熠 <caoyi@caoyideMacBook-Pro.local>
2026-04-23 15:02:12 +0800

    fix: api path import

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
-from trainquest_backend_a.app import app
+# api/index.py
+import sys
+import os
 
-# 给 Vercel 使用
+# 把项目根目录加入 Python 路径
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# 现在可以稳定导入
+from trainquest_backend_a.app import app
 application = app
